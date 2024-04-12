c58a30ad5ad57ba42380a277fe90e4420ac8f566
kbramh <kylebramhall2@hotmail.com>
2024-04-11 21:00:01 -0700

    feat: add dynamic chirp data

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
-import { createClient } from "@supabase/supabase-js";
-import React, { useEffect, useState } from "react";
+import { createClient } from '@supabase/supabase-js';
+import React, { useEffect, useState } from 'react';
 
 const supabase = createClient(
   import.meta.env.VITE_API_URL,
-  import.meta.env.VITE_API_KEY,
+  import.meta.env.VITE_API_KEY
 );
 
 export function App() {
   const [data, setData] = useState([]);
   useEffect(() => {
     supabase
-      .from("chirps")
-      .select("*, profiles!chirps_author_fkey ( * )")
+      .from('chirps')
+      .select('*, profiles!chirps_author_fkey ( * )')
       .then(({ data }) => {
         setData(data);
       });
   }, []);
 
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
       {data.map((chirp) => (
         <div className="frontnav" key={chirp.id}>
           <div className="frontnav-header">
-            Kyle Bramhall
-            <div className="frontnav-description">@kbramh</div>
+            {chirp.profiles.first_name} {chirp.profiles.last_name}
+            <div className="frontnav-description">
+              @{chirp.profiles.username}
+            </div>
           </div>
-          <div className="frontnav-content">First chirp</div>
+          <div className="frontnav-content">{chirp.content}</div>
           <div className="frontnav-footer">
             <i className="fa-solid fa-heart frontnav-action"></i>
             <i className="frontnav-chirp-data">1.2k</i>
