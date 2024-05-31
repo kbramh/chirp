9dd13baa956bb4d27ac6cc594def3cadff15499c
Zachary Dodge <zacharysdodge@gmail.com>
2024-05-30 20:25:19 -0700

    fix: refactor page layout into separate component

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const Home = (props) => {
+export default function Login(props) {
   const { loggedIn, email } = props;
   const navigate = useNavigate();
 
@@ -10,22 +10,22 @@ const Home = (props) => {
   };
 
   return (
-    <div className="mainContainer">
-      <div className={'titleContainer'}>
-        <div>Welcome!</div>
-      </div>
-      <div>This is the home page.</div>
-      <div className={'buttonContainer'}>
-        <input
-          className={'inputButton'}
-          type="button"
-          onClick={onButtonClick}
-          value={loggedIn ? 'Log out' : 'Log in'}
-        />
-        {loggedIn ? <div>Your email address is {email}</div> : <div />}
+    <div id="main">
+      <div className="mainContainer">
+        <div className="titleContainer">
+          <div>Welcome!</div>
+        </div>
+        <div>This is the home page.</div>
+        <div className="buttonContainer">
+          <input
+            className="inputButton"
+            type="button"
+            onClick={onButtonClick}
+            value={loggedIn ? 'Log out' : 'Log in'}
+          />
+          {loggedIn ? <div>Your email address is {email}</div> : <div />}
+        </div>
       </div>
     </div>
   );
 };
-
-export default Login;
